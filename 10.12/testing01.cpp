a613b4536a0e85f382958c1312095e63d0b95120
Phill Conrad <pconrad@cs.ucsb.edu>
2021-10-12 13:48:07 -0700

    pc - add testing01.cpp to 10.12 notes

---
+++ 10.12/testing01.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+string n_spaces(int length) {
+  string result;
+  for (int i=0; i<length; i++) {
+    result += " ";
+  }
+  return result;
+}
+
+void expect_check(string expected, string actual) {
+  if (expected == actual) {
+    cout << "PASSED" << endl;
+  } else {
+    cout << "  FAILED! expected: [" << expected << "] but got [" << actual <<"]" << endl;
+  }
+}
+
+int main() {
+   expect_check("",n_spaces(0));
+   expect_check(" ",n_spaces(1));
+   expect_check("     ",n_spaces(5));
+   return 0;
+}
