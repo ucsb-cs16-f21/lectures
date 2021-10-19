cc2c8d74e6d0a218bdd4e327d21ca72778b25b17
Phill Conrad <pconrad@cs.ucsb.edu>
2021-10-19 13:54:02 -0700

    pc - add pointers

---
+++ 10.19/pointers/pointers01.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+  int x = 42;
+  int y = 96;
+  int *a = &x;
+  int *b = &y;
+  
+  cout << "x = " << x << endl;
+  cout << "y = " << y << endl;
+  cout << "a = " << a << endl;
+  cout << "b = " << b << endl;
+
+  cout << "sizeof x = " << sizeof x << endl;
+  cout << "sizeof y = " << sizeof y << endl;
+  cout << "sizeof a = " << sizeof a << endl;
+  cout << "sizeof b = " << sizeof b << endl;
+  
+  cout << "&x = " << &x << endl;
+  cout << "&y = " << &y << endl;
+  cout << "&a = " << &a << endl;
+  cout << "&b = " << &b << endl;
+
+
+  
+  // cout << "*x = " << *x << endl;   /* syntax error */
+  cout << "*a = " << *a << endl;
+  
+  return 0;
+}
