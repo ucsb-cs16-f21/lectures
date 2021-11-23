6e1e1b546069d3573d499922238fbd822c38427f
Phill Conrad <pconrad@cs.ucsb.edu>
2021-11-23 14:58:52 -0800

    updates from 11/23 lecture

--- 11.23/11.02.review/heapVsStack.cpp
+++ 11.23/11.02.review/heapVsStack.cpp
@@ -1,4 +1,12 @@
-int main() {
+// argc, and argv are both parameters to the main function
+// and are therefore on the stack
+// x and p are local variables inside main, and are therefore on the stack
+// BUT... the integer that p points to after line 13,
+// that integer is on the heap.
+// Anything allocated with "new" is on the heap.
+
+
+int main(int argc, char *argv[]) {
     
     int x;
     int *p = &x;  // *p is on the stack
