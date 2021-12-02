5a251f7b2b072f8ba74afce0ec9d7040acc8131a
Phill Conrad <pconrad@cs.ucsb.edu>
2021-12-02 12:34:31 -0800

    pc - sumArrayArgv

---
+++ 12.02/arrayRec/sumArrayArgv.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <cstdlib> // for exit()
+using namespace std;
+
+
+int * argvToArray(int argc, char *argv[]) {
+  
+  if (argc <= 1) {
+    cerr << "Error: argvToArray expects argc >= 1" << endl;
+    exit(1); // 
+  }
+  
+  // allocate an array on the heap
+  // one fewer than argc, since argv[0] doesn't count
+  
+  int * result = new int[argc - 1];
+  
+  // Store argv[1] to argv[argc-1] in result[0] to result[argc-2]
+  for (int i=1; i<argc; i++) {
+    result[i-1]=atoi(argv[i]); 
+  }
+  
+  return result;  
+}
+
+void usage() {
+  cerr << "Usage: ./sumArray num1 num2 num2 ... numN" << endl;
+}
+
+
+int sumArray(int *nums, int size) {
+  if (size<=0)
+    return 0;
+  else
+    return nums[0] + sumArray(nums+1, size-1);
+}
+
+int main(int argc, char *argv[]) {
+
+  if (argc <= 1) {
+    usage();
+    exit(2);
+  }
+
+  int * nums = argvToArray(argc, argv);
+  int size = argc-1;
+
+  int sum = sumArray(nums, size);
+  cout << "sum=" << sum << endl;
+
+  return 0;
+}
